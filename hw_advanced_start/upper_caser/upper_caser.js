335e6909142fdf246e9cf8165926bab0bca4370c
Scott Johnston <scott@umachan.co.uk>
2021-02-10 17:29:40 +0000

    Initial commit

---
+++ hw_advanced_start/upper_caser/upper_caser.js
@@ -0,0 +1,20 @@
+const UpperCaser = function (words) {
+    this.words = words;
+}
+
+UpperCaser.prototype.toUpperCase = function () {
+    // const uppercaseWord = this.words.map((word) => {
+    //     return word.toUpperCase();
+    // })
+    // return uppercaseWord;
+
+    // Refactored
+    // return this.words.map((word) => {
+    //     return word.toUpperCase();
+    // })
+
+    //re-refactored
+    return this.words.map(word => word.toUpperCase())
+}
+module.exports = UpperCaser;
+
